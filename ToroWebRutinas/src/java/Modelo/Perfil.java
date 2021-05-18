0933cb76d8543871a85a70525e164220b93fa2ff
RequenaRojas <mrequenar1900@alumno.ipn.mx>
2021-05-18 12:11:46 -0500

    Modelo del perfil

---
+++ ToroWebRutinas/src/java/Modelo/Perfil.java
@@ -0,0 +1,96 @@
+
+package Modelo;
+
+import java.awt.Image;
+import java.sql.Date;
+
+/**
+ *
+ * @author sofo9
+ */
+public class Perfil {
+   int id_perf;
+   String nom_perf;
+   Image img_perf;
+   String email_per;
+   String contra_perf;
+   Date fechNaci_perf;
+   boolean admin;
+   boolean creador;
+   int id_biBli_crea;
+   
+   public void Perfil(){}
+
+    public int getId_perf() {
+        return id_perf;
+    }
+
+    public void setId_perf(int id_perf) {
+        this.id_perf = id_perf;
+    }
+
+    public String getNom_perf() {
+        return nom_perf;
+    }
+
+    public void setNom_perf(String nom_perf) {
+        this.nom_perf = nom_perf;
+    }
+
+    public Image getImg_perf() {
+        return img_perf;
+    }
+
+    public void setImg_perf(Image img_perf) {
+        this.img_perf = img_perf;
+    }
+
+    public String getEmail_per() {
+        return email_per;
+    }
+
+    public void setEmail_per(String email_per) {
+        this.email_per = email_per;
+    }
+
+    public String getContra_perf() {
+        return contra_perf;
+    }
+
+    public void setContra_perf(String contra_perf) {
+        this.contra_perf = contra_perf;
+    }
+
+    public Date getFechNaci_perf() {
+        return fechNaci_perf;
+    }
+
+    public void setFechNaci_perf(Date fechNaci_perf) {
+        this.fechNaci_perf = fechNaci_perf;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public boolean isCreador() {
+        return creador;
+    }
+
+    public void setCreador(boolean creador) {
+        this.creador = creador;
+    }
+
+    public int getId_biBli_crea() {
+        return id_biBli_crea;
+    }
+
+    public void setId_biBli_crea(int id_biBli_crea) {
+        this.id_biBli_crea = id_biBli_crea;
+    }
+    
+}
