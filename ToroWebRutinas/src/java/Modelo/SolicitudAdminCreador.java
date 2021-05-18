c7bcb245e3a9a69d59486a36485c530606d73722
RequenaRojas <mrequenar1900@alumno.ipn.mx>
2021-05-18 12:58:59 -0500

    Complete el modelo de Solicitud

---
+++ ToroWebRutinas/src/java/Modelo/SolicitudAdminCreador.java
@@ -0,0 +1,56 @@
+package Modelo;
+
+/**
+ *
+ * @author sofo9
+ */
+public class SolicitudAdminCreador {
+    int soli_AC;
+    int id_perf1;
+    int id_perf2;
+    String motivo;
+    boolean aceptado;
+
+    public int getSoli_AC() {
+        return soli_AC;
+    }
+
+    public void setSoli_AC(int soli_AC) {
+        this.soli_AC = soli_AC;
+    }
+
+    public int getId_perf1() {
+        return id_perf1;
+    }
+
+    public void setId_perf1(int id_perf1) {
+        this.id_perf1 = id_perf1;
+    }
+
+    public int getId_perf2() {
+        return id_perf2;
+    }
+
+    public void setId_perf2(int id_perf2) {
+        this.id_perf2 = id_perf2;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public boolean isAceptado() {
+        return aceptado;
+    }
+
+    public void setAceptado(boolean aceptado) {
+        this.aceptado = aceptado;
+    }
+    
+    
+    
+}
