9473817ad10f1cca6646a6a8be36ea201f78e8b1
KuChauWe <gomez.ocampo.hugo.1@gmail.com>
2021-06-16 21:33:35 -0500

    Vista de las rutinas y ejercicios

--- JS/susmain.js
+++ JS/susmain.js
@@ -53,18 +53,18 @@ window.addEventListener('load', function(e){
 		responsive: [
 			{
 			  // screens greater than >= 775px
-			  breakpoint: 450,
+			  breakpoint: 550,
 			  settings: {
 				// Set to `auto` and provide item width to adjust to viewport
-				slidesToShow: 2,
-				slidesToScroll: 2
+				slidesToShow: 1,
+				slidesToScroll: 1
 			  }
 			},{
 			  // screens greater than >= 1024px
 			  breakpoint: 800,
 			  settings: {
-				slidesToShow: 5,
-				slidesToScroll: 5,
+				slidesToShow: 3,
+				slidesToScroll: 3,
 				itemWidth: 150,
 				duration: 2
 			  }
